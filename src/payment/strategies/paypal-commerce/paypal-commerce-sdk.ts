a2a06ad796be1d73d8f9d31c70f19140ea9b52b4
andriiVitvitskyi1990 <andrii.vitrvitskyi@bigcommerce.com>
2020-12-21 14:48:23 +0200

    feat(payments): PAYPAL-APM_TEST

--- src/payment/strategies/paypal-commerce/paypal-commerce-sdk.ts
+++ src/payment/strategies/paypal-commerce/paypal-commerce-sdk.ts
@@ -180,6 +180,8 @@ export interface PaypalCommerceHostWindow extends Window {
 export interface PaypalCommerceInitializationData {
     clientId: string;
     merchantId?: string;
+    buyerCountry?: string;
+    isDeveloperModeApplicable?: boolean;
     intent?: 'capture' | 'authorize';
     isPayPalCreditAvailable?: boolean;
     isProgressiveOnboardingAvailable?: boolean;
@@ -193,6 +195,7 @@ export type ComponentsScriptType = Array<'buttons' | 'messages' | 'hosted-fields
 export interface PaypalCommerceScriptParams  {
     'client-id': string;
     'merchant-id'?: string;
+    'buyer-country'?: string;
     'disable-funding'?: DisableFundingType;
     'data-client-token'?: string;
     'partner-attribution-id'?: string;
